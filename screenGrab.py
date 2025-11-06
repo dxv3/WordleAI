7139353fab1c31dcb650116d7a43556ad62fd909
dxv3 <davesangha9@gmail.com>
2025-11-06 21:10:35 +0000

    Get each row

--- screenGrab.py
+++ screenGrab.py
@@ -10,6 +10,15 @@ def get_rows():
     # top left of board x584 y219
     # bottom right of board x884 y576
 
+    row_height = 357//6
+    # get each row
+    for i in range(6):
+        top = 219+i*row_height
+        bottom = top + row_height
+        row_img = whole_board.crop((0, i * row_height, 300, (i+1)*row_height))
+        row_img.save(f"images/rows/row_{i+1}.png")
+
+
 
 
 get_rows()
